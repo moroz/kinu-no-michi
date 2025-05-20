1d9957d9b27540ee11d491e5f6fae9d2f72364f1
Karol Moroz <karol@moroz.dev>
2025-05-20 09:27:49 +0800

    Set up navigation in layout

--- templates/helpers.go
+++ templates/helpers.go
@@ -1,1 +1,15 @@
 package templates
+
+type menuItem struct {
+	label string
+	href  string
+}
+
+var menuItems = []menuItem{
+	{"Home", "/"},
+	{"Fruit", "/fruit"},
+	{"Vegetables", "/vegetables"},
+	{"Books", "/books"},
+	{"Firearms", "/firearms"},
+	{"Cosmetics", "/cosmetics"},
+}
